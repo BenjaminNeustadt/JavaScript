e0d7fc2c53283caee8482096675fd1b6611fab8e
benjamin <b.james.neustadt@gmail.com>
2022-06-21 16:56:27 +0200

    Phase 1: Part 9 - Arrays in JavaScript
    Create an array of all numbers from 1 to 10.
    Calculate the sum of all these numbers by looping through the array.
    
    You can use the method forEach to iterate over each element.
    Although ex asks to use forEach, we found that reduce is better to use
    in this instance.

---
+++ 9_JavaScript_Arrays/1_exercise.js
@@ -0,0 +1,14 @@
+const names = ['Bob', 'Jon', 'Mike', 'Aisha'];
+
+const newNames = names.concat('Jesus');
+// console.log(names);
+// console.log(names.length);
+// console.log(names[1]);
+// console.log(newNames);
+
+
+// Create an array of a few names of people you know and:
+
+// 1.Print the array's length.
+// 2.Print the second name of the array.
+// 3.Create a new array by adding a new name to the existing array.
---
+++ 9_JavaScript_Arrays/2_exercise.js
@@ -0,0 +1,17 @@
+const numbers = [1, 2, 10, 10]
+
+// numbers.sum
+// numbers.forEach(n => console.log(n));
+
+// const sum = [1, 2, 3].reduce((partialSum, a) => partialSum + a, 0);
+// console.log(sum);
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+console.log(
+  numbers.reduce((a, b) => a + b)
+)
+
+
+// WHAT IS THE DIFFERENCE BETWEEN `forEach` and `reduce`? 
+
