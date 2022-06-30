98d2c5d296cb8efd653c63c212936c12a86906eb
benjamin <b.james.neustadt@gmail.com>
2022-07-01 01:22:07 +0200

    Created setPowertSaving(psm) mode
    Placed the logic inside the up() method.
    
    *Placed the logic in up() method,think about
     moving it to setPowerSavingMode(psm) method,
     for separation of concerns. Can it be put as a callback?
     and called from within the up method.
    
    *Created a reset() method that sets this.thermostat
     back to 20. Created this with TDD. tested
     (but deleted the case) to check that temp can be increased
     again after.

--- 2_phase_JavaScript/04_JS_thermostat/thermostat.js
+++ 2_phase_JavaScript/04_JS_thermostat/thermostat.js
@@ -1,6 +1,7 @@
 class Thermostat {
   constructor() {
     this.thermostat = 20
+    this.psm = true
   }
 
   getTemperature() {
@@ -8,15 +9,32 @@ class Thermostat {
   }
 
   up(){
-    this.thermostat = this.thermostat + 1
+    if (this.psm == true && this.thermostat < 25) {
+      this.thermostat += 1;
+    }else if (this.psm == false && this.thermostat < 32) {
+      this.thermostat += 1;
+    }
+  } 
+
+  // this is a loop
+  
+  //ASK AMIR ABOUT THIS
+
+  setPowerSavingMode(psm){
+    this.psm = psm;
   }
 
   down(){
-    this.thermostat = this.thermostat - 1
+    if (this.thermostat > 10){
+      this.thermostat = this.thermostat - 1
+    }else{
+      return this.thermostat
+    }
+  }
+  reset(){
+    this.thermostat = 20
   }
 }
-
-
 
 module.exports = Thermostat;
 
--- 2_phase_JavaScript/04_JS_thermostat/thermostat.test.js
+++ 2_phase_JavaScript/04_JS_thermostat/thermostat.test.js
@@ -20,22 +20,44 @@ describe('thermostat', () => {
     expect(thermostat.getTemperature()).toEqual(18);
   })
 
+  it ('does not decrease below 10', () => {
+    const thermostat = new Thermostat();
+    for (let i = 0 ; i < 10 ; i++) {
+      thermostat.down();
+    }
+    expect(thermostat.getTemperature()).toEqual(10);
+  })
 
+  it ('when PSM on, max temp is 25', () => {
+    const thermostat = new Thermostat();
+    thermostat.setPowerSavingMode(true)
+    for (let i = 0 ; i < 15 ; i++) {
+      thermostat.up();
+    }
+    expect(thermostat.getTemperature()).toEqual(25);
+  })
+
+  it ('when PSM off, max temp is 32, and temp can be higher than 25', () => {
+    const thermostat = new Thermostat();
+    thermostat.setPowerSavingMode(false)
+    for (let i = 0 ; i < 20 ; i++) {
+      thermostat.up();
+    }
+    expect(thermostat.getTemperature()).toEqual(32);
+  })
+
+  it ('when reset method is called, this.thermostat = 20', () => {
+    const thermostat = new Thermostat();
+    thermostat.setPowerSavingMode(false)
+    for (let i = 0 ; i < 20 ; i++) {
+      thermostat.up();
+    }
+    thermostat.reset();
+    expect(thermostat.getTemperature()).toEqual(20);
+  })
 })
 
 
-
-
-
-
-
-// thermostat.setPowerSavingMode(true); // PSM is now on, max temperature is 25
-
-// for (let i = 0 ; i < 10 ; i++) {
-//   thermostat.up();
-// }
-
-// thermostat.getTemperature(); // should be 25 (max reached)
 
 // thermostat.setPowerSavingMode(false); // PSM is now off, max temperature is no more 25
 
@@ -58,3 +80,11 @@ describe('thermostat', () => {
 
 // thermostat.down();
 // thermostat.getTemperature(); // should now return 21 - - - - - -> DONE
+
+// thermostat.setPowerSavingMode(true); // PSM is now on, max temperature is 25
+
+// for (let i = 0 ; i < 10 ; i++) {
+//   thermostat.up();
+// }
+
+// thermostat.getTemperature(); // should be 25 (max reached) - - - - - - - - DONE
